9885cff3622f0b81e3e14f0efa18ce8d1e549bce
AnaClaraprisma <an4claracouto@gmail.com>
2024-09-04 09:48:47 -0300

    aula de api

--- api/server.js
+++ api/server.js
@@ -1,7 +1,38 @@
 const http = require('http');
+const express = require("express");
+const server = express();
+
+server.get("/", (_request,response)=> {
+  response.status(200).end("ola mundo!")
+});
+server.get("/ping", (_request,response)=> {
+  response.status(200).end("pong")
+});
+//orta ue retorna todas as pessoas do objeto
+//porta que retorna uma pessoa em especifico
+server.get((_request,response)=> {
+  response.status(404).end("rota nao encontrada")
+});
+server.listen(3000, () =>console.log("servidor rodando!"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Função de callback para lidar com as requisições
-const requestListener = (req, res) => {
+/*const requestListener = (req, res) => {
   // Verifica se a rota solicitada é "/ping"
   if (req.url === '/ping') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -19,4 +50,4 @@ const server = http.createServer(requestListener);
 const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
-});
+});*/
